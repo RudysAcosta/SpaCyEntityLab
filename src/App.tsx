d1da96834d3d0fcec0508724436e59fe65d18650
Rudys Acosta <156624476+RudysAcosta@users.noreply.github.com>
2024-06-28 02:01:47 -0400

    Update App.tsx
    Delete code comment

--- src/App.tsx
+++ src/App.tsx
@@ -25,24 +25,6 @@ function App() {
       </TagProvider>
     </div>
 
-
-    // <div className="min-h-screen flex flex-col px-8 my-8 border bottom-1 shadow-lg rounded">
-    //   <header className="bg-blue-600 text-white p-4">
-    //     <h1 className="text-2xl font-bold text-left">Doc for training model in spacy</h1>
-    //   </header>
-    //   <main className="flex-grow">
-    //   <TagProvider>
-    //     <Tags />
-    //     <TextAreaProvider>
-    //       <TextArea />
-    //       <TextWithEntities />
-    //     </TextAreaProvider>
-    //   </TagProvider>
-    //   </main>
-    //   <footer className="bg-gray-800 text-white p-4 text-center">
-    //     © 2024 Mi Página.
-    //   </footer>
-    // </div>
   )
 }
 
